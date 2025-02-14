5e440aa32bbd02a793b8d5025a91545b89fe6ae7
Adrian Peiro <adrpeiser@alu.edu.gva.es>
2025-02-14 09:55:59 +0100

    aacabado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { EmpleadosComponent } from './empleados/empleados.component';
+import { EmpleadoComponent } from './empleado/empleado.component';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
+  selector: 'app-raiz',
+  imports: [RouterOutlet , EmpleadosComponent, EmpleadoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ProjectAngular';
+  saludo = "Hola lorena que tal estas";
+
 }
